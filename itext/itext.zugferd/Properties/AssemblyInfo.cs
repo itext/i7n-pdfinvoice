19e4dda3e1be81c68e74fdef8b0248d3b6f4c404
Pavel Alay <pavel.alay@duallab.com>
2017-10-31 13:56:37 +0300

    [RELEASE] 1.0.2-SNAPSHOT -> 1.0.3-SNAPSHOT

--- itext/itext.zugferd/Properties/AssemblyInfo.cs
+++ itext/itext.zugferd/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("cf45e61e-a3bc-4b64-aa92-5814af726286")]
 
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
-[assembly: AssemblyInformationalVersion("1.0.2-SNAPSHOT")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyInformationalVersion("1.0.3-SNAPSHOT")]
